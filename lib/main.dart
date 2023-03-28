86a5d565148a8581faedb0dd5c0b47c52d3e9c9b
sai-prasad-1 <sai.prasad.1603@gmail.com>
2023-03-28 21:19:05 +0530

    Modified Home Page

--- lib/main.dart
+++ lib/main.dart
@@ -70,45 +70,49 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/bg_img.png"),
+            fit: BoxFit.cover,
+          ),
         ),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Padding(
+              padding: const EdgeInsets.only(top: kToolbarHeight),
+              child: Padding(
+                padding: const EdgeInsets.all(50),
+                child: Image.asset(
+                  'assets/logo.png',
+                  fit: BoxFit.contain,
+                ),
+              )),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text('Push The Button to Confirm Amal Is a Killadee:',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: MediaQuery.of(context).size.width > 600 ? 30 : 10,
+                  fontWeight: FontWeight.bold,
+                )),
+          ),
+          Text('You Have confirmed Amal Is a Killadee $_counter Times',
+              style: TextStyle(
+                color: Colors.amber,
+                fontSize: MediaQuery.of(context).size.width > 600 ? 35 : 15,
+                fontWeight: FontWeight.bold,
+              )),
+        ]),
       ),
       floatingActionButton: FloatingActionButton(
+        
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        tooltip: 'Killadee Button',
+        child: 
+            const Icon(Icons.fire_extinguisher_sharp),
+          
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
